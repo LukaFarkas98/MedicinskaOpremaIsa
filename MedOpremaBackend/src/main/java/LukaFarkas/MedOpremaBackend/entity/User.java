f95a525028f30d7f729a9779e9b1d70ec5cfe44f
LukaFarkas98 <luka.farkas998@gmail.com>
2024-05-13 20:00:14 +0300

    added user class and fixed app properties file

---
+++ MedOpremaBackend/src/main/java/LukaFarkas/MedOpremaBackend/entity/User.java
@@ -0,0 +1,36 @@
+package LukaFarkas.MedOpremaBackend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "first_name")
+    private String FirstName;
+    @Column(name = "last_name")
+    private String LastName;
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "city")
+    private String city;
+    @Column(name = "country")
+    private String country;
+    @Column(name = "phone")
+    private String phone;
+    @Column(name = "profession")
+    private String profession;
+
+}
