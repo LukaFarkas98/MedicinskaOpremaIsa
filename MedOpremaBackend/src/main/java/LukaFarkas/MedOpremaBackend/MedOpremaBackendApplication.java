f7f7648bd99b24a42a03c1db1da68b46db35a674
LukaFarkas98 <luka.farkas998@gmail.com>
2024-05-13 19:00:23 +0300

    Initial commit

---
+++ MedOpremaBackend/src/main/java/LukaFarkas/MedOpremaBackend/MedOpremaBackendApplication.java
@@ -0,0 +1,13 @@
+package LukaFarkas.MedOpremaBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedOpremaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedOpremaBackendApplication.class, args);
+	}
+
+}
---
+++ MedOpremaBackend/src/test/java/LukaFarkas/MedOpremaBackend/MedOpremaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package LukaFarkas.MedOpremaBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedOpremaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
